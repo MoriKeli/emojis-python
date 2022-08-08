bc52b81eb0f024468dde231eec18728da02be54f
MoriKeli <kelimbth@gmail.com>
2022-08-08 22:44:38 +0300

    emoji

---
+++ emoji.py
@@ -0,0 +1,11 @@
+"""
+    © 2022 MORI KELI | ALL RIGHTS RESERVED.
+    Program created by Mori Keli
+    Created Sat 06-08-2022 2043hrs EAT
+"""
+
+print('\U0001F917')
+print('\U0001F62A')
+print('\U0001F637')
+print('\U0001F618')
+print('\U0001F600')
